8f80de45a04eeda387e2fd020cf9a4c2c6c818e8
Codigo Aldea <dmdndrd@gmail.com>
2022-02-24 16:49:49 +0530

    new

---
+++ programs/consonant.java
@@ -0,0 +1,16 @@
+package programs;
+import java.util.*;
+
+public class consonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the character :");
+        char ch = sc.next().toLowerCase().charAt(0);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ){
+            System.out.println("It is vowel");
+        }
+        else{
+            System.out.println("It is consonant");
+        }
+    }
+}
