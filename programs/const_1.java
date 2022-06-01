64145e3caabe054b32efc6c2c0a284327ded229e
Codigo Aldea <dmdndrd@gmail.com>
2022-06-01 16:04:21 +0530

    new

---
+++ programs/const_1.java
@@ -0,0 +1,15 @@
+package programs;
+
+/**
+ * abc
+ */ 
+class abc {
+    public abc() {
+        System.out.println("Hello World");
+    }
+}
+public class const_1 {
+    public static void main(String[] args) {
+        abc a = new abc();
+    }
+}
